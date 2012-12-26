21d2dae32e63dcbf2be60db32a7673b4fef19797
Cory Mikell <cory.mikell@gmail.com>
2012-12-26 13:30:06 -0500

    Rails app, bootstrap, styling

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '8c65071e8aacff24e1cdaa400d08320a030384d3b53e61d086684d01ef92cc08053df859eb4b60301878573666002298dc4f10e50d22173d4bae8c25b8b58a8b'
